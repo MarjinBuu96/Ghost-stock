3504a50ab555e5f66deadc28c9e88c0e1bbc22ed
Jordan Lewis <rapwriterstv@gmail.com>
2025-09-26 18:31:56 +0100

    commit

--- src/app/shopify/exit-iframe/page.tsx
+++ src/app/shopify/exit-iframe/page.tsx
@@ -1,21 +1,23 @@
-// src/app/shopify/exit-iframe/page.tsx
+"use client";
+
+import { useEffect } from "react";
+
 export const dynamic = "force-static";
 
 export default function ExitIframe() {
+  useEffect(() => {
+    const p = new URLSearchParams(window.location.search);
+    const target = p.get("target") || "/";
+    if (window.top === window.self) {
+      window.location.href = target;
+    } else {
+      window.top.location.href = target;
+    }
+  }, []);
+
   return (
-    <html>
-      <body>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              const p = new URLSearchParams(window.location.search);
-              const target = p.get('target') || '/';
-              if (window.top === window.self) location.href = target;
-              else window.top.location.href = target;
-            `,
-          }}
-        />
-      </body>
-    </html>
+    <div>
+      <p>Redirecting...</p>
+    </div>
   );
 }
