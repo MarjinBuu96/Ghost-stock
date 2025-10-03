9262437e203790a5c173848a3c1de330f9e12887
Jordan Lewis <rapwriterstv@gmail.com>
2025-10-04 00:11:32 +0100

    commit

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,5 +1,9 @@
 // src/app/dashboard.js
 "use client";
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
+export const runtime = "nodejs";
 
 import useSWR from "swr";
 import { fetcher } from "@/lib/fetcher";
